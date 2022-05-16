fbc8b3eed801e8da50eb755fbdcf6774d0a4376f
sihaixianyu <2532827403@qq.com>
2022-05-16 10:55:46 +0800

    2022-05-16

---
+++ main.py
@@ -0,0 +1,6 @@
+from collections import defaultdict
+
+if __name__ == '__main__':
+    d = defaultdict(int)
+    print(d[0])
+    print(d)
---
+++ structure/binary_tree.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class TreeNode:
+    def __init__(self):
+        pass
+
+
+class BinaryTree:
+    def __init__(self):
+        pass
+
+
+def levelorder_traverse(tree: BinaryTree) -> List[int]:
+    pass
+
+
+if __name__ == '__main__':
+    pass
